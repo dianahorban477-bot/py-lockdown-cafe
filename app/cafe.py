f46158e9981fa9f299ca19c7ead458e70a98e0e4
dianahorban <dianahorban477@gmail.com>
2026-04-26 18:01:50 +0200

    The solution

--- app/cafe.py
+++ app/cafe.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from app.errors import (NotVaccinatedError,
                         OutdatedVaccineError,
                         NotWearingMaskError)
